fc4534cc229b8be92d393070cf4bf497da458894
Ali Mohammed <110703574+Ali-M-Ba@users.noreply.github.com>
2024-06-22 08:18:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import router from './routes/routes.js';
+import session from 'express-session';
 
 const app = express();
 // process.env.PORT => if you want to deploy ur app to online server to use their default port
@@ -10,6 +11,13 @@ app.use(express.urlencoded({ extended: true }))
 
 // is a middleware function that is used to parse JSON data sent in the request body.
 app.use(express.json())
+
+app.use(session({
+  secret: 'very_strong_key', // replace with a strong secret key
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Set to true if using HTTPS
+}));
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
